03821de91efac28ecf5daa5c5914d1c8d4cc758e
danialgray <Danik34_dark@mail.ru>
2023-10-07 20:45:54 +0400

    create InputHandler

---
+++ app/src/main/java/com/graysoft/snakefromjs/InputHandler.java
@@ -0,0 +1,54 @@
+package com.graysoft.snakefromjs;
+
+import com.badlogic.gdx.InputProcessor;
+
+public class InputHandler implements InputProcessor{
+
+    @Override
+    public boolean keyDown(int arg0) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean keyUp(int arg0) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean keyTyped(char arg0) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean touchDragged(int arg0, int arg1, int arg2) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean mouseMoved(int arg0, int arg1) {
+        return false;
+    }
+    
+
+    @Override
+    public boolean scrolled(float arg0, float arg1) {
+        return false;
+    }
+    
+}
